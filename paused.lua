8f9f979b6fc1ff6a6d440637d8991f34e8d8304f
wendeldavid <wendel.przygoda@rdstation.com>
2025-11-14 11:28:37 -0300

    Update pause menu input handling for different build types

--- paused.lua
+++ paused.lua
@@ -6,18 +6,12 @@ function paused:draw()
     love.graphics.setColor(1, 1, 1)
     local resume_key = 'START'
     if BUILD_TYPE == 'linux' then resume_key = 'ENTER' end
-    if BUILD_TYPE == 'nx' then resume_key = '-' end
+    if BUILD_TYPE == 'nx' then resume_key = '+' end
     love.graphics.printf("Game Paused\nPress '" .. resume_key .. "' to Resume", 0, 480 / 2 - 20, 640, 'center')
 end
 
 function paused:keypressed(key)
-    if key == 'p' then
-        Gamestate.pop()
-    end
-end
-
-function paused:gamepadpressed(gamepad, button)
-    if button == 'start' then
+    if (BUILD_TYPE == 'linux' and  key == 'p') or (BUILD_TYPE == 'nx' and key == '+') then
         Gamestate.pop()
     end
 end
